912e5785dd2334a7cda2510be6c82de394a85287
seaConjurer7 <sweetatious27@gmail.com>
2019-06-30 02:02:05 -0700

    Adding timer logic. Program runs and relays time elapsed but cannot store anything yet.

--- horologium.py
+++ horologium.py
@@ -3,5 +3,53 @@
 
 # 07.01.2019 Carsen Weinzetl
 
+# Initializing modules and classes
 import time_module
+Timer = time_module.Timer()
 
+# Title printout
+print('''
+ _                     _             _                 
+| |__   ___  _ __ ___ | | ___   __ _(_)_   _ _ __ ___  
+| '_ \ / _ \| '__/ _ \| |/ _ \ / _` | | | | | '_ ` _ \ 
+| | | | (_) | | | (_) | | (_) | (_| | | |_| | | | | | |
+|_| |_|\___/|_|  \___/|_|\___/ \__, |_|\__,_|_| |_| |_|
+                               |___/                   
+-------------------------------------------------------
+	''')
+
+# List options
+options = '''
+Things you can do:
+start > starts to record time
+log > log your time and reset the timer
+quit > exit the program and log your progress
+options > show this list
+'''
+print(options)
+
+#
+# Prompt the user with a list of commands
+usr_input = input('What would you like to do? ')
+
+# Disseminate commands
+while usr_input:
+
+    if usr_input == 'quit':
+        '''Stop the timer if started, log the time, and exit'''
+        break
+
+    elif usr_input == 'start':
+        '''Begin the timer'''
+        Timer.timer('start')
+        break
+
+    elif usr_input == 'log':
+        '''Log the elapsed time'''
+        print('Logging progress...')
+        # Timer.timer('log') // This is broken for the time being
+        break
+
+    elif usr_input == 'options':
+        print(options)
+        break
--- time_module.py
+++ time_module.py
@@ -1,24 +1,46 @@
 # Module for all functions to be used by 'horologium' to record
 # and return time spent programming
 
-Class Records():
-    '''Stores users and their time spent'''
+import time
+
+
+class Records:
+    '''Stores users and their logged time'''
 
     def __init__(self):
-    	'''Initialize dictionary of users'''
+        '''Initialize dictionary of users'''
 
-    	self.users = {}
+        self.users = {}
 
     def write_time(self, time):
-    	'''Writing the recorded time to the give users dictionary'''
+        '''Writing the recorded time to the give users dictionary'''
 
 
-Class Timer():
-	'''Class that holds the logic behind the time recordings'''
+class Timer:
+    '''Class that holds the logic behind the time recordings'''
 
-	def __init__(self):
+    def __init__(self):
+        '''Initialize time on timer'''
 
-	def timer(self, input):
-		'''According to input, start or stop the timer'''
+    def timer(self, command):
+        '''According to input, start or stop the timer'''
+        total_time = 0
 
-		
+        if command == 'start':
+            # Prints the different between the start/stop as
+            # the elapsed time
+            start = time.time()
+            end = input('Type "stop" to stop the timer ')
+
+            if end == 'stop':
+                stop = time.time()
+                elapsed = stop - start
+
+                # Print the elapsed time
+                print('You have worked for, '
+                      + str(round(elapsed))
+                      + ' seconds.')
+
+        elif command == 'log':
+            '''Writing the total time to the dictionary'''
+            print('Logging time...')
