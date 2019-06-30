e9d1563a53935c236aaf657d42ec816d415ec93c
seaConjurer7 <sweetatious27@gmail.com>
2019-06-29 21:58:36 -0700

    Building up part of the skeleton for the program

---
+++ horologium.py
@@ -0,0 +1,7 @@
+# A terminal program that can record and display,
+# your time spent programming
+
+# 07.01.2019 Carsen Weinzetl
+
+import time_module
+
---
+++ time_module.py
@@ -0,0 +1,24 @@
+# Module for all functions to be used by 'horologium' to record
+# and return time spent programming
+
+Class Records():
+    '''Stores users and their time spent'''
+
+    def __init__(self):
+    	'''Initialize dictionary of users'''
+
+    	self.users = {}
+
+    def write_time(self, time):
+    	'''Writing the recorded time to the give users dictionary'''
+
+
+Class Timer():
+	'''Class that holds the logic behind the time recordings'''
+
+	def __init__(self):
+
+	def timer(self, input):
+		'''According to input, start or stop the timer'''
+
+		
